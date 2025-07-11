0d3ccecb2dd13e6f08400dbdc8d2fccb010a635b
Bijoy Kar <bijoykar54321@gmail.com>
2025-07-11 13:30:57 +0530

    update: Refactor go.mod and go.sum for new dependencies; enhance MongoDB connection handling in script.go

--- cli/script.go
+++ cli/script.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/spf13/cobra"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 var rootCmd = &cobra.Command{
@@ -44,10 +49,59 @@ func printInfo(msg string, args ...interface{}) {
 	fmt.Printf("Info: "+msg+"\n", args...)
 }
 
+type User struct {
+	Email  string `bson:"email" json:"email"`
+	ApiKey string `bson:"apiKey" json:"apiKey"`
+}
+
 func main() {
 	if err := Execute(); err != nil {
 		printError("Failed to execute command: %v", err)
 		os.Exit(1)
 	}
-	printInfo("ccfmt started successfully")
+
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		printWarning("No .env file found, using default environment variables")
+	}
+	// Check if MONGODB_URL is set
+	MONGODB_URL := os.Getenv("MONGODB_URL")
+	if MONGODB_URL == "" {
+		printWarning("MONGODB_URL not set in .env, using default value")
+		os.Exit(1)
+	}
+	// Connect to MongoDB
+	client, err := mongo.Connect(options.Client().
+		ApplyURI(MONGODB_URL))
+	if err != nil {
+		panic(err)
+	}
+	// close the connection when done
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		} else {
+			printInfo("MongoDB connection closed successfully")
+		}
+	}()
+	// Ping the MongoDB server to verify connection
+	coll := client.Database("winget").Collection("users")
+	// Define the email to search for
+	email := "example@example.com"
+
+	var result bson.M
+	err = coll.FindOne(context.TODO(), bson.M{"email": email}).Decode(&result)
+
+	if err == mongo.ErrNoDocuments {
+		fmt.Printf("No document was found with the email %s\n", email)
+		return
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result)
+
+	printInfo("Connected to MongoDB successfully")
 }
